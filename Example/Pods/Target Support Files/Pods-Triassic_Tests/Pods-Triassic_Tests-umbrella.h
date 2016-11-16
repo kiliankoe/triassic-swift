f0736d9464c383c4e38b3d9d2dba9817e9eec3ce
Kilian Koeltzsch <me@kilian.io>
2016-11-16 15:44:56 +0100

    Update everything to Swift 3 compatibility

---
+++ Example/Pods/Target Support Files/Pods-Triassic_Tests/Pods-Triassic_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Triassic_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Triassic_TestsVersionString[];
+
