fdb8f266509fb1f4c5f2d9c9c6b78014a8b953f0
Kilian Koeltzsch <me@kilian.io>
2016-11-17 00:00:21 +0100

    Add SimpleModesSupport
    also rename SimpleUtility (filename collisions are stupid)

---
+++ Triassic/Classes/DataTypes/ModesSupport/ModesSupport.swift
@@ -0,0 +1,12 @@
+//
+//  ModesSupport.swift
+//  Pods
+//
+//  Created by Kilian Költzsch on 16/11/2016.
+//
+//
+
+import Foundation
+
+/// Types used for the classification of transport modes.
+public enum ModesSupport {}
