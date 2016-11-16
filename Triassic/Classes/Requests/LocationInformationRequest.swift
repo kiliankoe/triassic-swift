34417f74e5b49b7de3ebafac6efd76911dc52895
Kilian Koeltzsch <me@kilian.io>
2016-11-16 22:23:34 +0100

    Create stub for LocationInformationRequest

---
+++ Triassic/Classes/Requests/LocationInformationRequest.swift
@@ -0,0 +1,20 @@
+//
+//  LocationInformationRequest.swift
+//  Pods
+//
+//  Created by Kilian Költzsch on 16/11/2016.
+//
+//
+
+import Foundation
+
+/// Der Ortsinformationsdienst umfasst diese Funktionalitäten:
+/// - Start-/Ziel-Identifikation bei Eingabe einer Zeichenkette
+/// - Objektinformationsdienst zum Abrufen aller Ortsobjekte
+/// - Geografischer Kontextdienst zum Abrufen von Ortsobjekten in einem Kartenausschnitt
+/// - Koordinaten-zu-Adressdienst zum Abrufen der nächsten Adresse für gegebene Koordinaten
+/// - Abruf der nächsten Haltestellen(n) für gegebene Koordinaten
+/// - Ortsabhängiges Patternmatching einer Zeichenkette durch Berücksichtigung von gleichzeitig übergebenen Koordinaten
+public struct LocationInformationRequest: Request {
+
+}
---
+++ Triassic/Classes/Requests/Request.swift
@@ -0,0 +1,11 @@
+//
+//  Request.swift
+//  Pods
+//
+//  Created by Kilian Költzsch on 16/11/2016.
+//
+//
+
+import Foundation
+
+public protocol Request {}
