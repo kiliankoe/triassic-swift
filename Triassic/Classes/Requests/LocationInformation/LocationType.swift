4914b339413abff41e69702ed03440aa93b32660
Kilian Koeltzsch <me@kilian.io>
2016-11-17 01:11:36 +0100

    Add LocationType and LocationUsage

---
+++ Triassic/Classes/Requests/LocationInformation/LocationType.swift
@@ -0,0 +1,14 @@
+//
+//  LocationType.swift
+//  Pods
+//
+//  Created by Kilian Költzsch on 17/11/2016.
+//
+//
+
+import Foundation
+
+/// Typ eines Ortsobjektes.
+public enum LocationType {
+    case stop, address, poi, coord, locality
+}
---
+++ Triassic/Classes/Requests/LocationInformation/LocationUsage.swift
@@ -0,0 +1,14 @@
+//
+//  LocationUsage.swift
+//  Pods
+//
+//  Created by Kilian Költzsch on 17/11/2016.
+//
+//
+
+import Foundation
+
+/// Verwendungszweck eines Ortsobjektes.
+public enum LocationUsage {
+    case origin, destination, via
+}
