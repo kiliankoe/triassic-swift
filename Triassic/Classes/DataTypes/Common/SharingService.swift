a9e8745cf90ac4c5683481c83639d10e4a8d16f6
Kilian Koeltzsch <me@kilian.io>
2016-11-17 15:15:13 +0100

    Add Common.SharingService

---
+++ Triassic/Classes/DataTypes/Common/SharingService.swift
@@ -0,0 +1,37 @@
+//
+//  SharingService.swift
+//  Pods
+//
+//  Created by Kilian Költzsch on 17/11/2016.
+//
+//
+
+import Foundation
+
+public extension Common {
+    /// Struktur zur Beschreibung eines
+    public struct SharingService {
+        /// Operator-ID.
+        let `operator`: OperatorCode
+
+        /// Name des Mobilitätsdienstes.
+        let name: String?
+
+        /// Typ des Ausleih- und Rückgabeverfahrens.
+        let sharingModel: SharingModel?
+
+        /// Typische Zeit, die ein Benutzer einplanen muss, um sich am System anzumelden und das Fahrzeug in Fahrbereitschaft zu versetzen.
+        let timeBufferBefore: Int? // FIXME: Should be `xs:duration`, is also wrapped in `SharingServiceUsage` somehow according to spec
+
+        /// Typische Zeit, die ein Benutzer einplanen muss, um das Fahrzeug ordnungsgemäß abzustellen, zu verschließen und sich am System abzumelden.
+        let timeBufferAfter: Int? // FIXME: Should be `xs:duration`, is also wrapped like timeBufferBefore
+
+        /// Link zu Web-Seite mit weiterführenden Informationen.
+        let infoURL: Utility.WebLink?
+    }
+
+    /// Typ des Ausleih- und Rückgabeverfahrens.
+    public enum SharingModel {
+        case singleStationBased, multipleStationBased, nonStationBased
+    }
+}
