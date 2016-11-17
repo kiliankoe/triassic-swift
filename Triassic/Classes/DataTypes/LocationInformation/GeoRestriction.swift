11ceed7bfd15202070d118dee740b586a66e7d95
Kilian Koeltzsch <me@kilian.io>
2016-11-17 10:55:12 +0100

    Implement GeoRestriction

--- Triassic/Classes/DataTypes/LocationInformation/GeoRestriction.swift
+++ Triassic/Classes/DataTypes/LocationInformation/GeoRestriction.swift
@@ -10,5 +10,30 @@ import Foundation
 
 /// Definiert einen geografischen Filter.
 public struct GeoRestriction {
-    // TODO: Implement me with some kind of Either type or protocol conformance (s. 75)
+    /// Der Filter wird durch einen Kreis definiert.
+    let circle: GeoCircle?
+
+    /// Der Filter wird durch ein Rechteck definiert.
+    let rectangle: GeoRectangle?
+
+    /// Der Filter wird durch ein Polygon definiert.
+    let area: GeoArea?
+
+    init(circle: GeoCircle) {
+        self.circle = circle
+        self.rectangle = nil
+        self.area = nil
+    }
+
+    init(rectangle: GeoRectangle) {
+        self.circle = nil
+        self.rectangle = rectangle
+        self.area = nil
+    }
+
+    init(area: GeoArea) {
+        self.circle = nil
+        self.rectangle = nil
+        self.area = area
+    }
 }
