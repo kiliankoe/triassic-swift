9a25dd001ad5b9b1011add1a4a7c6d3bdd231c4a
Kilian Koeltzsch <me@kilian.io>
2016-11-17 11:59:56 +0100

    Add some whitespace

--- Triassic/Classes/DataTypes/Utility/Utility.swift
+++ Triassic/Classes/DataTypes/Utility/Utility.swift
@@ -13,26 +13,37 @@ import Foundation
 public enum Utility {
     /// Prozentangabe als Ganzzahlwert. Maximalwert ist 100.
     public typealias Percent = UInt
+
     /// Prozentangabe als Ganzzahlwert, nach oben unbeschränkt.
     public typealias OpenPercent = UInt
+
     /// Zeichenkette, die nur aus Nullen und Einsen bestehen kann.
     public typealias BitString = String
+
     /// Typ zur Angabe von Distanzen (in Metern).
     public typealias Distance = UInt
+
     /// Typ zur Angabe von Längen (in Metern).
     public typealias Length = UInt
+
     /// Typ zur Angabe einer Geschwindigkeit (in Meter pro Sekunde).
     public typealias Speed = UInt
+
     /// Prioritätswerte von 1 (höchste Priorität) bis 5 (niedrigste Priorität).
     public typealias Priority = UInt
+
     /// Geographische Länge.
     public typealias Longitude = Double
+
     /// Geographische Breite.
     public typealias Latitude = Double
+
     /// Höhe über dem Meeresspiegel in Meter.
     public typealias Altitude = Double
+
     /// Kompassrichtung in Grad. Nord = 0 Grad, im Uhrzeigersinn aufsteigende Werte.
     public typealias AbsoluteBearing = UInt
+
     /// Typ zur Angabe einer Telefonnummer.
     public typealias PhoneNumber = String
 }
