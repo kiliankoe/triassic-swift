004bda56354704aadea55c958f39301428d3402f
Kilian Koeltzsch <me@kilian.io>
2016-11-21 16:01:09 +0100

    Add some minimal testing content to play with

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -2,48 +2,13 @@
 
 import Quick
 import Nimble
-import Triassic
+@testable import Triassic
 
-class TableOfContentsSpec: QuickSpec {
+class PlaySpec: QuickSpec {
     override func spec() {
-        describe("these will fail") {
-
-            it("can do maths") {
-//                expect(1) == 2
-            }
-
-            it("can read") {
-//                expect("number") == "string"
-            }
-
-            it("will eventually fail") {
-//                expect("time").toEventually( equal("done") )
-            }
-            
-            context("these will pass") {
-
-                it("can do maths") {
-                    expect(23) == 23
-                }
-
-                it("can read") {
-                    expect("🐮") == "🐮"
-                }
-
-                it("will eventually pass") {
-                    var time = "passing"
-
-                    DispatchQueue.main.async {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        Thread.sleep(forTimeInterval: 0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
-                }
+        describe("Triassic.play") {
+            it("should output something") {
+                Triassic.play()
             }
         }
     }
--- Triassic/Classes/Triassic/Triassic.swift
+++ Triassic/Classes/Triassic/Triassic.swift
@@ -7,7 +7,14 @@
 //
 
 import Foundation
+import AEXML
 
 public class Triassic {
+    static func play() {
+        let doc = AEXMLDocument()
+        let attributes = ["xmlns": "trias", "xmlns:siri": "http://www.siri.org.uk/siri", "version": "1.0"]
+        let trias = doc.addChild(name: "Trias", attributes: attributes)
 
+        print(doc.xml)
+    }
 }
