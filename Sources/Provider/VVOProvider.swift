9ecbafdd93ffe8ecce458db2b830e5219e4b9c53
Kilian Koeltzsch <me@kilian.io>
2016-11-29 01:26:19 +0100

    add extension reminder to vvoprovider

--- Sources/Provider/VVOProvider.swift
+++ Sources/Provider/VVOProvider.swift
@@ -1,9 +1,9 @@
 //
 //  VVOProvider.swift
-//  Pods
+//  Triassic
 //
-//  Created by Kilian Költzsch on 16/11/2016.
-//
+//  Created by Kilian Költzsch on 29/11/2016.
+//  Copyright © 2016 Triassic Park. All rights reserved.
 //
 
 import Foundation
@@ -14,3 +14,5 @@ public struct VVOProvider: Provider {
     public let baseURL = URL(string: "http://trias.vvo-online.de:9000/Middleware/Data/Trias")!
     public let center = CLLocationCoordinate2D(latitude: 51.0532443, longitude: 13.7414791)
 }
+
+// TODO: Add extension for additional VVO stuff like routechanges via webapi
