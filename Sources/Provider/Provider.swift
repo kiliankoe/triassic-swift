fa5a9f0b2d1211917b44ba6d447a837f2964ff34
Kilian Koeltzsch <me@kilian.io>
2016-11-29 13:55:29 +0100

    Add send request func to providers
    also make current properties static

--- Sources/Provider/Provider.swift
+++ Sources/Provider/Provider.swift
@@ -1,16 +1,30 @@
 //
 //  Provider.swift
-//  Pods
+//  Triassic
 //
 //  Created by Kilian Költzsch on 16/11/2016.
-//
+//  Copyright © 2016 Triassic Park. All rights reserved.
 //
 
 import Foundation
 import CoreLocation
 
 public protocol Provider {
-    var id: Common.OperatorCode { get }
-    var baseURL: URL { get }
-    var center: CLLocationCoordinate2D { get }
+    static var id: Common.OperatorCode { get }
+    static var baseURL: URL { get }
+    static var center: CLLocationCoordinate2D { get }
+
+    static func send(_ request: Request, completion: @escaping (TriassicError?) -> Void)
+}
+
+public extension Provider {
+    static func send(_ request: Request, completion: @escaping (TriassicError?) -> Void) {
+        var urlRequest = URLRequest(url: self.baseURL)
+        urlRequest.httpMethod = "POST"
+        // TODO: Append `request` XML to `urlRequest` body
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            print(data ?? "no data received")
+            completion(nil)
+        }
+    }
 }
--- Sources/Provider/VVOProvider.swift
+++ Sources/Provider/VVOProvider.swift
@@ -10,9 +10,9 @@ import Foundation
 import CoreLocation
 
 public struct VVOProvider: Provider {
-    public let id = "VVO"
-    public let baseURL = URL(string: "http://trias.vvo-online.de:9000/Middleware/Data/Trias")!
-    public let center = CLLocationCoordinate2D(latitude: 51.0532443, longitude: 13.7414791)
+    public static let id = "VVO"
+    public static let baseURL = URL(string: "http://trias.vvo-online.de:9000/Middleware/Data/Trias")!
+    public static let center = CLLocationCoordinate2D(latitude: 51.0532443, longitude: 13.7414791)
 }
 
 // TODO: Add extension for additional VVO stuff like routechanges via webapi
