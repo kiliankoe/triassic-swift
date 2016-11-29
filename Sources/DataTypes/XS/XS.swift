1e95c459e4b6572b9725d15ca50a98a515ae2b64
Kilian Koeltzsch <me@kilian.io>
2016-11-29 15:34:39 +0100

    Create custom xs datatypes

--- Sources/DataTypes/XS/XS.swift
+++ Sources/DataTypes/XS/XS.swift
@@ -1,18 +1,104 @@
 //
 //  XS.swift
-//  Pods
+//  Triassic
 //
 //  Created by Kilian Költzsch on 22/11/2016.
-//
+//  Copyright © 2016 Triassic Park. All rights reserved.
 //
 
 import Foundation
 
 public enum XS {
-    // TODO: Figure out if these should be their own restrictive types or if this is enough.
-    public typealias NameToken = String
     public typealias Language = String
-    public typealias NormalizedString = String
-    public typealias DateTime = String
-    public typealias Duration = String // PnYnMnDTnHnMnS
+
+    /// Contains a string with line feeds, carriage returns, tabs, leading and trailing spaces, and multiple spaces removed.
+    public struct NameToken: CustomStringConvertible {
+        internal let value: String
+
+        init(_ value: String) {
+            self.value = value
+        }
+
+        public var description: String {
+            return value.condensedWhitespace
+        }
+    }
+
+    /// Contains a string with line feeds, carriage returns, and tab characters removed.
+    public struct NormalizedString: CustomStringConvertible {
+        internal let value: String
+
+        init(_ value: String) {
+            self.value = value
+        }
+
+        public var description: String {
+            return value
+                .replacingOccurrences(of: "\t", with: " ")
+                .replacingOccurrences(of: "\n", with: "")
+        }
+    }
+
+    /// Specify a time and date.
+    public struct DateTime: CustomStringConvertible {
+        internal let value: Date
+
+        internal static var dateFormatter: DateFormatter = {
+            let df = DateFormatter()
+            df.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+            return df
+        }()
+
+        init(_ value: Date) {
+            self.value = value
+        }
+
+        init?(_ value: String) {
+            guard let date = DateTime.dateFormatter.date(from: value) else {
+                return nil
+            }
+            self.value = date
+        }
+
+        public var description: String {
+            return DateTime.dateFormatter.string(from: self.value)
+        }
+    }
+
+    /// Specify a time interval.
+    public struct Duration: CustomStringConvertible {
+        internal let years: UInt?
+        internal let months: UInt?
+        internal let days: UInt?
+        internal let hours: UInt?
+        internal let minutes: UInt?
+        internal let seconds: UInt?
+
+        internal let isNegative: Bool
+
+        init(years: UInt? = nil, months: UInt? = nil, days: UInt? = nil, hours: UInt? = nil, minutes: UInt? = nil, seconds: UInt? = nil, isNegative: Bool = false) {
+            self.years = years
+            self.months = months
+            self.days = days
+            self.hours = hours
+            self.minutes = minutes
+            self.seconds = seconds
+            self.isNegative = isNegative
+        }
+
+        public var description: String {
+            // PnYnMnDTnHnMnS
+            var str = isNegative ? "-P" : "P"
+            if let years = years { str += "\(years)Y" }
+            if let months = months { str += "\(months)M" }
+            if let days = days { str += "\(days)D" }
+            if hours != nil || minutes != nil || seconds != nil {
+                str += "T"
+            }
+            if let hours = hours { str += "\(hours)H" }
+            if let minutes = minutes { str += "\(minutes)M" }
+            if let seconds = seconds { str += "\(seconds)S" }
+            return str
+        }
+    }
 }
--- Sources/Provider/VVOProvider.swift
+++ Sources/Provider/VVOProvider.swift
@@ -10,7 +10,7 @@ import Foundation
 import CoreLocation
 
 public struct VVOProvider: Provider {
-    public static let id = "VVO"
+    public static let id = XS.NameToken("VVO")
     public static let baseURL = URL(string: "http://trias.vvo-online.de:9000/Middleware/Data/Trias")!
     public static let center = CLLocationCoordinate2D(latitude: 51.0532443, longitude: 13.7414791)
 }
