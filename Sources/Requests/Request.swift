0413359dfcbc02c89858e58eec18c869b49299f3
Kilian Koeltzsch <me@kilian.io>
2016-11-30 10:54:07 +0100

    Add servicerequest and requestpayload wrappers
    obviously makes total sense to have these...

--- Sources/Requests/Request.swift
+++ Sources/Requests/Request.swift
@@ -1,11 +1,19 @@
 //
 //  Request.swift
-//  Pods
+//  Triassic
 //
 //  Created by Kilian Költzsch on 16/11/2016.
-//
+//  Copyright © 2016 Triassic Park. All rights reserved.
 //
 
 import Foundation
+
+public struct ServiceRequest {
+    public let payload: RequestPayload
+}
+
+public struct RequestPayload {
+    public let request: Request
+}
 
 public protocol Request {}
